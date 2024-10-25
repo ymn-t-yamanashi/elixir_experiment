483f1b4e69fc31d0ea3774b407139f146a6483a8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-25 15:58:56 +0900

    Enum.drop

--- test/elixir_experiment_test.exs
+++ test/elixir_experiment_test.exs
@@ -55,5 +55,7 @@ defmodule ElixirExperimentTest do
     assert Enum.dedup([1, 2, 3, 3, 4, 5, 5]) == [1, 2, 3, 4, 5]
 
     assert Enum.dedup_by(1..10, fn x -> x > 2 end) == [1, 3]
+
+    assert Enum.drop(1..10, 2) == [3, 4, 5, 6, 7, 8, 9, 10]
   end
 end
